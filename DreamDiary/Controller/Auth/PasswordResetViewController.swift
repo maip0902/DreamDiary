1af62426a820634828d94e7bfb9d3c285a588f61
maikanda <maiptomato@gmail.com>
2020-05-08 23:48:07 +0900

    パスワードリセット機能

--- DreamDiary/Controller/Auth/PasswordResetViewController.swift
+++ DreamDiary/Controller/Auth/PasswordResetViewController.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class passwordResetViewController: UIViewController {
 
+    @IBOutlet weak var emailAddress: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func send(_ sender: Any) {
+        if let email = emailAddress {
+            let verifyEmailAddress = email.text
+            Auth.auth().sendPasswordReset(withEmail: verifyEmailAddress!) { error in
+                if error != nil {
+                    return
+                }
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
