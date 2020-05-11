a5372549867eb1cff2a9818b1a2934618f18a503
maikanda <maiptomato@gmail.com>
2020-05-11 15:53:02 +0900

    フッター修正

--- DreamDiary/Controller/Diary/DiaryDetailViewController.swift
+++ DreamDiary/Controller/Diary/DiaryDetailViewController.swift
@@ -5,9 +5,15 @@ class DiaryDetailViewController: UIViewController {
 
     var diary: Diary?
     
+    @IBOutlet weak var footer: UIView!
     @IBOutlet weak var dateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let screenWidth = UIScreen.main.bounds.size.width
+        let screenHeight = UIScreen.main.bounds.size.height
+        let layout = CustomLayout(screenWidth, screenHeight)
+        layout.setFooter(footer)
         dateLabel?.text = diary?.date
         // Do any additional setup after loading the view.
     }
